f505c4a5deb68e21ac8d41baca8f79e40c51f32b
Earl Savadera <earl.john.savadera@gmail.com>
2020-11-28 14:17:43 +0800

    Add script for printing all uptrends start date in all stocks

--- backtest/minervini_criteria.py
+++ backtest/minervini_criteria.py
@@ -1,5 +1,9 @@
+import os
+from time import time
+
 import backtrader
 import pandas
+from tqdm import tqdm
 
 
 class Minervini(backtrader.Strategy):
@@ -10,30 +14,50 @@ class Minervini(backtrader.Strategy):
 
         if self.sample >= 50:
             self.sma50 = backtrader.indicators.SMA(period=50)
+        else:
+            self.sma50 = 0
         if self.sample >= 150:
             self.sma150 = backtrader.ind.SMA(period=150)
+        else:
+            self.sma150 = 0
         if self.sample >= 200:
             self.sma200 = backtrader.ind.SMA(period=200)
+        else:
+            self.sma200 = 0
 
     def next(self):
-        if self.sma50 > self.sma150 and self.sma150[0] > self.sma200[0] and not self.uptrend:
-            if self.datas[0].close[0] > self.sma50:
+        if self.sma50 > self.sma150 and self.sma150 > self.sma200 and not self.uptrend:
+            if self.datas[0].close[0] > self.sma50 and self.datas[0].volume[0] > 5000000:
                 self.buy()
                 self.uptrend = True
                 print(self.datas[0].datetime.date(0))
 
-        if self.sma200[0] > self.sma150[0] and self.uptrend:
+        if self.sma200 > self.sma150 and self.uptrend:
             self.sell()
             self.uptrend = False
 
 
 if __name__ == '__main__':
-    cerebro = backtrader.Cerebro()
-    dataframe = pandas.read_csv("stocks/ACEN.csv", header=0, parse_dates=["CHART_DATE"])
-    cerebro.addstrategy(Minervini)
+    t = time()
+    stocks = os.listdir("stocks")
+    cnt = 0
+    for stock in tqdm(stocks):
+        cerebro = backtrader.Cerebro()
+        cerebro.addstrategy(Minervini)
+        print(stock)
+        try:
+            dataframe = pandas.read_csv("stocks/" + stock, header=0, parse_dates=["CHART_DATE"])
+        except pandas.errors.EmptyDataError:
+            continue
+        if dataframe.shape[0] < 1:
+            continue
 
-    data = backtrader.feeds.PandasData(dataname=dataframe, datetime=3, open=0, volume=1, close=2, high=4, low=5)
-    cerebro.adddata(data)
+        cnt += 1
+        data = backtrader.feeds.PandasData(dataname=dataframe, datetime=3, open=0, volume=1, close=2, high=4, low=5)
+        cerebro.adddata(data)
 
-    cerebro.run()
-    cerebro.plot(style="bar")
+        cerebro.run()
+        # cerebro.plot(style="bar")
+
+    print("Total stocks", cnt)
+    print(int((time() - t) * 1000), "ms")
