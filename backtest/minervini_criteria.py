a241347b58395d3ed02956ef87137cb8cc50f2b1
Earl Savadera <earl.john.savadera@gmail.com>
2020-11-28 17:36:05 +0800

    Add output dict to manipulate the results

--- backtest/minervini_criteria.py
+++ backtest/minervini_criteria.py
@@ -7,10 +7,17 @@ from tqdm import tqdm
 
 
 class Minervini(backtrader.Strategy):
+    params = (
+        ("output", {}),
+        ("stock", ""),
+    )
 
     def __init__(self):
         self.sample = len(self.data.array)
         self.uptrend = False
+
+        self.params.output[self.params.stock] = []
+        self.out = self.params.output[self.params.stock]
 
         if self.sample >= 50:
             self.sma50 = backtrader.indicators.SMA(period=50)
@@ -26,11 +33,12 @@ class Minervini(backtrader.Strategy):
             self.sma200 = 0
 
     def next(self):
-        if self.sma50 > self.sma150 and self.sma150 > self.sma200 and not self.uptrend:
+        if self.sma50 > self.sma150 > self.sma200 and not self.uptrend:
             if self.datas[0].close[0] > self.sma50 and self.datas[0].volume[0] > 5000000:
                 self.buy()
                 self.uptrend = True
-                print(self.datas[0].datetime.date(0))
+
+                self.out.append(self.datas[0].datetime.date(0))
 
         if self.sma200 > self.sma150 and self.uptrend:
             self.sell()
@@ -40,24 +48,22 @@ class Minervini(backtrader.Strategy):
 if __name__ == '__main__':
     t = time()
     stocks = os.listdir("stocks")
-    cnt = 0
+    output = {}
     for stock in tqdm(stocks):
+        name = stock.split(".")[0]
         cerebro = backtrader.Cerebro()
-        cerebro.addstrategy(Minervini)
-        print(stock)
+        cerebro.addstrategy(Minervini, output=output, stock=name)
         try:
             dataframe = pandas.read_csv("stocks/" + stock, header=0, parse_dates=["CHART_DATE"])
         except pandas.errors.EmptyDataError:
             continue
-        if dataframe.shape[0] < 1:
-            continue
 
-        cnt += 1
         data = backtrader.feeds.PandasData(dataname=dataframe, datetime=3, open=0, volume=1, close=2, high=4, low=5)
         cerebro.adddata(data)
 
         cerebro.run()
         # cerebro.plot(style="bar")
 
-    print("Total stocks", cnt)
+    print(output)
+    print("Total stocks", len(output))
     print(int((time() - t) * 1000), "ms")
