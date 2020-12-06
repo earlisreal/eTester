b0f6c23a8f6cdd5bbb5ef28cfa6cee5c334e106b
Earl Savadera <earl.john.savadera@gmail.com>
2020-12-06 17:31:50 +0800

    Format output as tab separated

--- backtest/ema_exit.py
+++ backtest/ema_exit.py
@@ -1,31 +1,37 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import backtrader
 import pandas
 
 
-class EMA_Exit(backtrader.Strategy):
+class EMAExit(backtrader.Strategy):
     params = (
         ("entry", None),
-        ("ema_period", 5)
+        ("exits", {})
     )
 
     def __init__(self):
         self.entry = self.params.entry
-        self.ema = backtrader.indicators.SMA(period=self.params.ema_period)
-        self.end = False
+        self.exits = self.params.exits
+
+        self.indicators = {
+            "ema5": backtrader.indicators.EMA(period=5),
+            "ema10": backtrader.indicators.EMA(period=10),
+            "ema20": backtrader.indicators.EMA(period=20),
+            "sma10": backtrader.indicators.SMA(period=10),
+            "sma20": backtrader.indicators.SMA(period=20),
+            "sma50": backtrader.indicators.SMA(period=50),
+        }
 
     def next(self):
-        # print(self.datas[0].datetime.date(0))
         if self.datas[0].datetime.date(0) == self.entry:
             print("Buy Executed!")
             self.buy()
 
-        print(self.datas[0].datetime.date(0), self.datas[0].close[0], self.ema[0])
-        if self.position and self.datas[0].close[0] < self.ema:
-            print("Sell Executed!")
-            self.sell()
-            self.end = True
+        for indicator in self.indicators.keys():
+            if self.position and self.datas[0].close[0] < self.indicators[indicator][0]:
+                if indicator not in self.exits:
+                    self.exits[indicator] = {"date": self.datas[0].datetime.date(0), "price": self.datas[0].close[0]}
 
 
 if __name__ == '__main__':
@@ -36,19 +42,24 @@ if __name__ == '__main__':
         tokens = line.split("\t")
 
         entry = datetime.fromisoformat(tokens[1]).date()
+        exits = {}
 
         cerebro = backtrader.Cerebro()
-        cerebro.addstrategy(EMA_Exit, entry=entry)
+        cerebro.addstrategy(EMAExit, entry=entry, exits=exits)
 
         try:
             dataframe = pandas.read_csv("stocks/" + tokens[0] + ".csv", header=0, parse_dates=["CHART_DATE"])
         except pandas.errors.EmptyDataError:
             continue
 
-        data = backtrader.feeds.PandasData(fromdate=entry - timedelta(days=10), todate=entry + timedelta(days=60), dataname=dataframe, datetime=3, open=0, volume=1, close=2, high=4, low=5)
+        data = backtrader.feeds.PandasData(dataname=dataframe, datetime=3, open=0, volume=1, close=2, high=4, low=5)
         cerebro.adddata(data)
 
         cerebro.run()
-        # TODO: Save all EMA price and date to dict then compute manually the percentage
+
+        out = ""
+        for value in exits.values():
+            out += str(value["date"]) + "\t" + str(value["price"]) + "\t"
+        print(out)
 
     print("bye")
